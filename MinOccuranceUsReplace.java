c7ebc9a8b41c4e557b2be20457bc4de83421120f
Bharath <bharath23695a0508@gmail.com>
2026-05-25 13:21:10 +0530

    Count the Minimum Occurance of Given String

---
+++ MinOccuranceUsReplace.java
@@ -0,0 +1,22 @@
+class MinOccuranceUsReplace{
+    public static void main(String[] args) {
+        String s1="abbcccdddd";
+        char minChar=' ';
+        int minCount=s1.length();
+        while(s1.length()>0)
+        {
+            char ch=s1.charAt(0);
+            String s2=s1.replace(ch+"","");
+            int count=s1.length()-s2.length();
+            if(count<minCount)
+            {
+                minCount=count;
+                minChar=ch;
+            }
+            System.out.println(ch+"="+count);
+            s1=s2;
+        }
+        System.out.println("===============");
+        System.out.println(minChar+" : "+minCount);
+    }
+}
